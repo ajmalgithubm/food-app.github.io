57bb0649714feabe75bc318c04d03151190d77b9
ajmalgithubm <ajmalm76774@gmail.com>
2023-07-23 15:24:21 +0530

    react youtube is added

--- src/Component/MoreDetail/MoreDetail.js
+++ src/Component/MoreDetail/MoreDetail.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import  YouTube ,{YouTubeProps} from 'react-youtube'
 import './MoreDetail.css'
 import { useContext } from 'react';
 import { menuCategoryContext } from '../../App';
 function MoreDetail() {
+    let [menuDetailObj, setMenuDetailObj] =useState();
+    let [youtubeId, setYoutubeId] =useState();
     let data = useContext(menuCategoryContext);
     const opts = {
         height:'400',
@@ -12,32 +14,49 @@ function MoreDetail() {
             autoplay: 0,
         },
     };
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${data.menuDetail}`).then((response) => response.json()).then((data) => {
+            setMenuDetailObj(data.meals[0])
+            let youtubeLink = data.meals[0].strYoutube;
+            let id = youtubeLink.slice(32);
+            setYoutubeId(id);
+        })
+    },[])
 
     const onPlayerReady = (event) => {
         console.log('Player is ready:', event.target);
     };
     return (
         <div>
-            <div className="more-detail">
-                <div className="more-detail-head">
-                    <h1>{data.menuDetail}</h1>
-                </div>
-                <div className="more-detail-img">
-                    <img src="" alt="" />
-                </div>
-                <div className="more-detail-prepare">
-                    <h1>Recipe</h1>
-                    <p>Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.In a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.Drain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.</p>
-                </div>
-                <div className="more-detail-ingrediants">
-                    <h1>Ingrediants</h1>
-                    <li>Sugar - 1/2 cup</li>
-                    <li>Rice - 1/2 cup</li>
-                </div>
-                <div className="more-detail-youtube">
-                    <YouTube videoId="2g811Eo7K8U"  opts={opts} onReady={onPlayerReady} />
-                </div>
-            </div>
+            {
+                menuDetailObj && (<div className="more-detail">
+                    <div className="more-detail-head">
+                        <h1>{menuDetailObj.strMeal}</h1>
+                    </div>
+                    <div className="more-detail-img">
+                        <img src={menuDetailObj.strMealThumb} alt="" />
+                    </div>
+                    <div className="more-detail-prepare">
+                        <h1>Recipe</h1>
+                        <p>{menuDetailObj.strInstructions}</p>
+                    </div>
+                    <div className="more-detail-ingrediants">
+                        <h1>Ingrediants</h1>
+                        {
+                            [0,1,2,3,4,5,6,7,8].map((item) => {
+                                return(
+                                    <li></li>
+                                )
+                            })
+                        }
+                        <li>Sugar - 1/2 cup</li>
+                        <li>Rice - 1/2 cup</li>
+                    </div>
+                    <div className="more-detail-youtube">
+                        <YouTube videoId={youtubeId} opts={opts} onReady={onPlayerReady} />
+                    </div>
+                </div>)
+            }
         </div>
     )
 }
